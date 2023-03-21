89e8a5a990e24d4ebd32f3b93b02668f1d0469a5
@NelProgrammer <nelsonlzweni@gmail.com>
2023-03-21 11:32:00 +0200

    Split date to separate component: Component - ExpenseDate, CSS:ExpenseDate.css. then call Date inside ExpenseItem Component, and call ExpenseItem from App. Note data is propagated via the properties of the relevant Tag to the child component.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-  const expYear = props.date.getFullYear();
-  const expMonth = props.date.toLocaleString('en-SA', { month: 'long' });
-  const expDay = props.date.toLocaleString('en-SA', { day: '2-digit' });
-  const expDate = expDay + ' ' + expMonth + ' ' + expYear;
-
   return (
     <div className="expense-item">
-      <div className="expense-item__date">{expDate}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">R{props.amount}</div>
