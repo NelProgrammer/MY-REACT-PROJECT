727896a3efbfdab1982ca703e353abaec9a6a192
@NelProgrammer <nelsonlzweni@gmail.com>
2023-03-24 19:14:45 +0200

    Added event handline via multiple hooks & handlers.

--- src/components/UpdateLogic/ExpenseForm.js
+++ src/components/UpdateLogic/ExpenseForm.js
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState(''); // Start with no values
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  const [enteredFormData, setEnteredFormData] = useState('');
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const clickHandler = (event) => {
+    setEnteredFormData(enteredAmount & ',' & enteredTitle & ',' & enteredDate);
+    console.log(event.target);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2023-01-01" max="2023-03-24" />
+          <input
+            type="date"
+            min="2023-01-01"
+            max="2023-03-24"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+        <button type="submit" onClick={clickHandler}>
+          Add Expense
+        </button>
       </div>
     </form>
   );
