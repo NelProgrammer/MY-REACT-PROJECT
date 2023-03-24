597e5393d7551d59840eab0cdc82614dc9c51403
@NelProgrammer <nelsonlzweni@gmail.com>
2023-03-24 17:15:03 +0200

    Add Hooks - use State

--- src/components/Logic/ExpenseItem.js
+++ src/components/Logic/ExpenseItem.js
@@ -1,16 +1,24 @@
-import React from 'react'; // You only need this for compatibility with old React Code
+import React, { useState } from 'react'; // You only need this for compatibility with old React Code
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle(props.title + ' Updated via Hooks');
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">R{props.amount}</div>
       </div>
+      <button onClick={clickHandler}> Change Title </button>
     </Card>
   );
 };
