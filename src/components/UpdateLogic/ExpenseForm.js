20d73a4d7d0f0ffdb184f5ced601bcd11c6aeee9
@NelProgrammer <nelsonlzweni@gmail.com>
2023-03-24 20:59:20 +0200

    Added EventHandler for the form: 1. Form Submit Handler & overriding. Title & Amount Handler. 2 Way Binding using value property assigned to the Hook input variable. Enabled cleaning component after clicing to submit the Form.

--- src/components/UpdateLogic/ExpenseForm.js
+++ src/components/UpdateLogic/ExpenseForm.js
@@ -2,42 +2,57 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  // You can chose between individual state or agreegated.
-  //   const [enteredTitle, setEnteredTitle] = useState(''); // Start with no values
-  //   const [enteredAmount, setEnteredAmount] = useState('');
-  //   const [enteredDate, setEnteredDate] = useState('');
-  //   const [enteredFormData, setEnteredFormData] = useState('');
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    setEnteredAmount: '',
-    setEnteredDate: '',
-  });
+  //   You can chose between individual state or agreegated.
+  const [enteredTitle, setEnteredTitle] = useState(''); // Start with no values
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  // const [enteredFormData, setEnteredFormData] = useState('');
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: '',
+  //     setEnteredAmount: '',
+  //     setEnteredDate: '',
+  //   });
+
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
-    setUserInput({ ...userInput, enteredTitle: event.target.value }); /// Destruction is to retain other state values ...userInput
+    setEnteredTitle(event.target.value);
+    // setUserInput({ ...userInput, enteredTitle: event.target.value }); /// Destruction is to retain other state values ...userInput
   };
 
   const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
-    setUserInput({ ...userInput, setEnteredAmount: event.target.value });
+    setEnteredAmount(event.target.value);
+    // setUserInput({ ...userInput, setEnteredAmount: event.target.value });
   };
 
   const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
-    setUserInput({ ...userInput, setEnteredDate: event.target.value });
+    setEnteredDate(event.target.value);
+    // setUserInput({ ...userInput, setEnteredDate: event.target.value });
   };
 
-  const clickHandler = (event) => {
-    // setEnteredFormData(enteredAmount & ',' & enteredTitle & ',' & enteredDate);
-    // console.log(event.target);
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -45,6 +60,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -54,14 +70,13 @@ const ExpenseForm = () => {
             type="date"
             min="2023-01-01"
             max="2023-03-24"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="submit" onClick={clickHandler}>
-          Add Expense
-        </button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
