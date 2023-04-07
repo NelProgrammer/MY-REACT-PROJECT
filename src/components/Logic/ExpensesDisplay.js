ddef1761403afba5789d30a9c2032bc3f00a752c
@NelProgrammer <nelsonlzweni@gmail.com>
2023-04-07 10:01:05 +0200

    Change Array from Fixed to dynamic using Map function. Note the Component and it's attributes need to be passed returned in a function.

--- src/components/Logic/ExpensesDisplay.js
+++ src/components/Logic/ExpensesDisplay.js
@@ -22,26 +22,13 @@ const ExpensesDisplay = (props) => {
           onFilterYearData={dropDownFilterChange}
         />
         <p>{yearChange}</p>
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        {props.expenses.map((expenseItem) => (
+          <ExpenseItem
+            title={expenseItem.title}
+            amount={expenseItem.amount}
+            date={expenseItem.date}
+          />
+        ))}
       </Card>
     </div>
   );
