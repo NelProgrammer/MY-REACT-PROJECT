08c6b7a595d5e416bc0830fc024aee88ab9f5059
@NelProgrammer <nelsonlzweni@gmail.com>
2023-04-07 08:59:54 +0200

    Working Solution. (A) Get Data from the downstream component. 1. You need to create a Fake event in the upstream component needing data: onFilterYearData = {dropDownFilterChange}. 2. You then send this down beanstalk to be called, like laying a Landmine 3. When a a landmine is triggered by a downstream event, data is passed to it's handler's parameters. 4. This data now becomes available up stream's handler's argument.
    (B) Event Handler of the DownStream component.
    1. This handler needs to step on the landmine and send data up the beanstalk.
    2. This data is in an Event passed as an Argument of the Fake Event declated upstream.
    3. To get access to this even you need pull it inside the props.
    
    (C). Using data that is availble upstream.
    1. The data from down the bneanstalk is available in the argument. It was passed when landmine was triggered by the downstream component.
    2. The you need to use setState to coppy it to the rendering end.
    3. The rendering end does not have access to this argument as it is local to that Event Handler function.
    3. Important to remember using setState does not give you use of the updated Data, it is available only in the rendering section.
    
    (D). Using the Data downn steam or other components downsteam.
    1. Pass it to a Props then call the Props where needed. selectedYear & props.selectedYear.
    
    End of Commit

--- src/components/Logic/ExpensesDisplay.js
+++ src/components/Logic/ExpensesDisplay.js
@@ -1,22 +1,27 @@
-import React from 'react'; // You only need this for compatibility with old React Code
+import React, { useState } from 'react'; // You only need this for compatibility with old React Code
 import './ExpensesDisplay.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from '../FilterLogic/ExpensesFilter';
 
 const ExpensesDisplay = (props) => {
-  const FilterYearData = (selectedYearFilterData) => {
-    const yearFilterData = selectedYearFilterData;
-
-    console.log('In ExpenseDisplay.js => ExpensesDisplay => FilterYearData');
-    console.log('Year Filder Data:' + yearFilterData);
-    console.log('Selected Year Filder Data:' + selectedYearFilterData);
+  const [yearChange, setYearChange] = useState('2022');
+  // This Handler uses it's parameter to fetch data downrange
+  const dropDownFilterChange = (selectedYearFilterData) => {
+    setYearChange(selectedYearFilterData);
+    console.log('ExpenseDisplay.js => ExpensesDisplay => dropDownFilterChange');
+    console.log(' - Selected Year Filder Data:' + selectedYearFilterData);
+    console.log(' - yearChange - after setHook:' + yearChange); // Data Copy is avaiable only on return/rerendeing not now and also available when you resend it down beanstalf
   };
   return (
     <div>
-      <ExpensesFilter onFilterYearData={FilterYearData} />
-
       <Card className="expenses">
+        <ExpensesFilter
+          selectedYear={yearChange} // Data fetchhed downranged is now used
+          // Created below Event/wormhole to send it down beanstalk to fetch component Data
+          onFilterYearData={dropDownFilterChange}
+        />
+        <p>{yearChange}</p>
         <ExpenseItem
           title={props.expenses[0].title}
           amount={props.expenses[0].amount}
